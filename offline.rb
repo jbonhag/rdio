5c36768830b52e56212be588a03b8465a984e175
Jeff Bonhag <jeff@thebonhags.com>
2014-12-07 21:58:47 -0500

    Do not download overrepresented artists
    If an artist shows up multiple times in the offline tracks,
    put them on hold (don't download their music).

--- offline.rb
+++ offline.rb
@@ -2,6 +2,30 @@ $LOAD_PATH << '.'
 require 'rdio'
 require 'dotenv'
 
+def number_of_tracks_by_artist(collection, artist)
+  collection.select {|t| t["artist"] == artist }.length
+end
+
+def do_not_download_overrepresented_artists(rdio)
+  offline_tracks = rdio.call('getOfflineTracks', {"count" => 200})['result']
+
+  # nice suggestion from jmonteiro on Stack overflow
+  overrepresented_artist_tracks = offline_tracks.select do |track| 
+    number_of_tracks_by_artist(offline_tracks, track["artist"]) > 1
+  end
+
+  do_not_download = overrepresented_artist_tracks.collect { |t| t["key"] }
+
+  if do_not_download.length > 0
+    puts "Putting the following artists on hold:"
+    do_not_download.collect {|t| t["artist"]}.uniq.each do |artist|
+      puts artist
+    end
+  end
+
+  rdio.call('setAvailableOffline', {"keys" => do_not_download.join(','), "offline" => false})
+end
+
 Dotenv.load
 
 rdio = Rdio.new([ENV['RDIO_CONSUMER_KEY'], ENV['RDIO_CONSUMER_SECRET']])
@@ -22,7 +46,4 @@ if rdio.token == [nil, nil]
   end
 end
 
-offline_tracks = rdio.call('getOfflineTracks')['result']
-
-puts offline_tracks.inspect
-
+do_not_download_overrepresented_artists(rdio)
