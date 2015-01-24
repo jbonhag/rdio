1f8a87a94f24da3af927af61cb3aa7a1ed20147a
Jeff Bonhag <jbonhag@sca-corp.com>
2015-01-24 12:46:31 -0500

    Search for tracks

--- src/rdio/api.clj
+++ src/rdio/api.clj
@@ -1,8 +1,7 @@
 (ns rdio.api
   (:require [clj-http.client :as http]
             [environ.core :refer [env]]
-            [oauth.client :as oauth]
-            [clojure.java.browse :refer :all]))
+            [oauth.client :as oauth]))
 
 (def rdio-consumer-key
   (env :rdio-consumer-key))
@@ -23,3 +22,17 @@
                                    "https://www.rdio.com/account/oauth1/authorize/"
                                    :hmac-sha1))
 
+(defn search [query]
+  (def form-params {:method "search", :query query :types "track"})
+
+  (def credentials (oauth/credentials consumer
+                                      rdio-token
+                                      rdio-token-secret
+                                      :POST
+                                      "http://api.rdio.com/1/"
+                                      form-params))
+
+  (http/post "http://api.rdio.com/1/"
+             {:form-params (merge credentials form-params)}))
+
+(search "blake mills")
