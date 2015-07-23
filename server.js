801ffd16f8cb36e63919128db5426ceb3cfda200
Jeff Bonhag <jeff@thebonhags.com>
2015-07-23 19:35:22 -0400

    refactor

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var fs = require('fs');
-var system = require('system');
 
 var filePath = [fs.workingDirectory, 'index.html'].join(fs.separator);
 
-var webserverTest = require("webserver").create();
-webserverTest.listen(8083, function(request, response) {
-  webserverTest.registerFile('/', filePath);
+var server = require("webserver").create();
+server.listen(8083, function(request, response) {
+  server.registerFile('/', filePath);
 });
