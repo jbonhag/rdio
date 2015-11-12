22781eb0c21b59af74589a622fe26a38a30018e5
Jeff Bonhag <jeff@thebonhags.com>
2015-11-11 22:24:36 -0500

    Plain text from server

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
 var fs = require('fs');
-
-var filePath = [fs.workingDirectory, 'index.html'].join(fs.separator);
-
 var server = require("webserver").create();
+var page = require('webpage').create();
+
+server.registerFile('/index.html', fs.workingDirectory + '/index.html');
+server.registerFile('/rdio.js', fs.workingDirectory + '/rdio.js');
+
 server.listen(8083, function(request, response) {
-  server.registerFile('/', filePath);
+  if (request.method === 'POST') {
+    page.evaluate(function(key) {
+      document.getElementById('apiswf').rdio_play(key);
+    }, 't2996238');
+    response.setHeader('Content-Type', 'text/plain');
+    response.write('OK');
+    response.close()
+  }
 });
+
+console.log('server done');
+
+
+page.viewportSize = { width: 1, height: 1 };
+
+page.open('http://localhost:8083/index.html', function (status) {
+  if (status == "success") {
+    console.log("The title of the page is: "+ page.title);
+  }
+  else {
+    console.log("Sorry, the page is not loaded");
+  }
+});
+
+console.log('page done');
